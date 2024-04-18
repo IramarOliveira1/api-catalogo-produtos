9a516cc03b0bf8b0d725d92a2f37a1c2a8708b78
iramaroliveira <iramar.oliveira@trc.sebraeba.com.br>
2024-04-18 09:19:51 -0300

    first commit

---
+++ src/test/java/br/com/cairu/projeto/integrador/brecho/BrechoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cairu.projeto.integrador.brecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrechoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
